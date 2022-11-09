f667253c60b0c5c0d29dd23b131b9338a4bd18c8
Akash Joshi <akashjoshirm.aj@gmail.com>
2022-11-09 19:17:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 from BankNotes import BankNote
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from imblearn.over_sampling import SMOTE
-from sklearn.preprocessing import LabelEncoder
+
 
 import pickle
 
 app = FastAPI()
 pickle_in = open('classifier_new.pkl' ,'rb')
 classifier = pickle.load(pickle_in)
-df = pd.read_csv('Training Data.csv')
-df.rename(columns={"Married/Single": "Married_Single"}, inplace=True)
-encoder = LabelEncoder()
-df["Married_Single"] = encoder.fit_transform(df["Married_Single"])
-df["Car_Ownership"] = encoder.fit_transform(df["Car_Ownership"])
-df = df.drop(['Id', 'Profession', 'CITY', 'STATE', 'House_Ownership'], axis=1)
-X = df.drop("Risk_Flag", axis=1)
-y = df["Risk_Flag"]
-smote = SMOTE(sampling_strategy='minority')
-X_sm_original, y_sm_original = smote.fit_resample(X, y)
-sc = StandardScaler()
-sc.fit(X_sm_original, y_sm_original)
+
 
 
 @app.get('/')
@@ -49,20 +35,20 @@ def predict_species(data:BankNote):
     # print(classifier.predict([[balance , AnnualSalary , Employed]]))
     print("Hello")
 
-    Income = data['Income']
-    Age = data['Age']
-    Experience = data['Experience']
+    Income = (data['Income'] - 10310)/(9999938-10310)
+    Age = (data['Age'] - 21)/(79-21)
+    Experience = (data['Experience']) /20
     Married_Single = data['MarriedSingle']
     Car_Ownership  = data['Car_Ownership']
-    CURRENT_JOB_YRS = data['CurrentJob_YRS']
-    CURRENT_HOUSE_YRS = data['CurrentHour_YRS']
+    CURRENT_JOB_YRS = data['CurrentJob_YRS']/14
+    CURRENT_HOUSE_YRS = (data['CurrentHour_YRS']-10)/14
 
-    # sc.transform([Income , Age , Experience , Married_Single , Car_Ownership , CURRENT_JOB_YRS , CURRENT_HOUSE_YRS ])
-    prediction = classifier.predict([[sc.transform([[Income , Age , Experience , Married_Single , Car_Ownership , CURRENT_JOB_YRS , CURRENT_HOUSE_YRS ]])]])
+
+    prediction = classifier.predict([[Income , Age , Experience , Married_Single , Car_Ownership , CURRENT_JOB_YRS , CURRENT_HOUSE_YRS]])
     if(prediction[0] == 0):
-        prediction = "Not Approved"
-    else:
         prediction = "Approved"
+    else:
+        prediction = "Not Approved"
     return {
         "prediction": prediction
     }
